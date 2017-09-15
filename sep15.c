30e4a0327c46b203ce7ae943fd99eb9db830a4f6
yiduoke <yiduoke822@jhs185.org>
2017-09-15 13:47:16 -0400

    done

--- sep15.c
+++ sep15.c
@@ -11,6 +11,28 @@ int problemOne(){
 	return sum;
 }
 
+int isPrime(int y){//1 not prime; 0 for is prime
+	int x;
+	for (x=2; x<y; x++){
+		if (y%x==0){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int sumPrime(){
+	int sum=0;
+	int y;
+	for (y=2; y<2000000; y++){
+		if (isPrime(y)==0){
+			sum+=y;
+		}
+	}
+	return sum;
+}
+
 int main(){
 	printf("%d \n",problemOne());
+	printf("%d \n",sumPrime());
 }
